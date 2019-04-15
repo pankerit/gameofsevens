93c7898a7061588cea5d6c0b9d019274c16fb650
Costea <pankerbest@gmail.com>
2019-04-15 05:18:33 +0300

    s8e1

--- src/data.js
+++ src/data.js
@@ -563,7 +563,7 @@ export default [
         ep: 1,
         img:
           'https://www.telegraph.co.uk/content/dam/tv/2019/03/06/Winterfell-battle-game-of-thrones-season-8_trans_NvBQzQNjv4BqfqW6igm2KYus3Tnvc1kUsGreqapuikfNyCYuptSHoIo.jpg?imwidth=480',
-        title_en: 'Dragonstone'
+        title_en: 'Game of Thrones 68'
       },
     ]
   }
@@ -644,6 +644,6 @@ export const episodes = [
     'https://openload.co/embed/Du44dZvMf0I/game.of.thrones.s07e06.720p.web.h264-tbs.mkv.mp4',
     'https://openload.co/embed/v9ADAnMuliA/Game.of.Thrones.S07E07.The.Dragon.and.the.Wolf.720p.AMZN.WEB-DL.DDP5.1.H.264-GoT.mkv.mp4',],
   [
-    'https://yastatic.net/yandex-video-player-iframe-api-bundles/1.0-573/index.html?post_message_config=true&stream_url=https%3A%2F%2Fstreaming.disk.yandex.net%2Fhls%2FU2FsdGVkX1-YiFfUEVo1k5bha8zDkZNpEm1wV-3M-Mbb4Q_BDg8MWXcUKT5CHJMKi1BSoCo_lhVAvOLLs-0Vw5VRsV-khtJ_z6jgt0kI7tbpT4Dautdt6cishZpVqBoNFW7_TePCq8hhAR3KPt9k7Hou6G3CYetJoz7QwQmqMj-xrbYeRouizxgVP3NMwmDL593K9BQB_-D8Fef7KQPXrDUJMBDHFPmYKmS0UnlAUUocVlsQUZ4b1bh3PJQ2ZhFYnUbW7tHnRcjM-XvEutqjSpl4xEoLY6joSNe5IdW3s8I%2F586950372bef0%2F3433cd5ac0a182cc60c901a68ff1e2c62adfa61870f60bc4801cd32035e87b4f%2Fmaster-playlist.m3u8&volume=39&muted=false&auto_quality=false&report=false&report_url=https%3A%2F%2Fyandex.ru%2Fsupport%2Fteletranslation%2Fform.html&preview=%2F%2F4.downloader.disk.yandex.ru%2Fpreview%2Fb08dad356757f8459400a7d820ff97c0c74a6da464eadeba33887a7a956bbc67%2Finf%2F3vrPEEQWkCGw62AooUiSsS9uqgrKhM7XRlZ2QSlm224PSJ-T1crEuCHxMA88sW6UyOuZIn0-Sqba5g0TGb5wnA%253D%253D%3Fuid%3D306257568%26filename%3DGame.Of.Thrones.S01E01.Winter.Is.Coming.1080p.HDTV.x264-DunHD.mkv%26disposition%3Dinline%26hash%3D%26limit%3D0%26content_type%3Dimage%252Fjpeg%26tknv%3Dv2%26size%3D1920x1072&host=disk.yandex.ru&additional_params=%7B%22from%22%3A%22other%22%2C%22vsid%22%3A%224cf2c5c95cbc51a494dde999a3dcca9423d1941ea525c01b460d993b46e69b7a%22%7D&vsid=4cf2c5c95cbc51a494dde999a3dcca9423d1941ea525c01b460d993b46e69b7a',
+    'https://yastatic.net/yandex-video-player-iframe-api-bundles/1.0-573/index.html?post_message_config=true&stream_url=https%3A%2F%2Fstreaming.disk.yandex.net%2Fhls%2FU2FsdGVkX18yMxvDViMzdWHgQNNz7HvYiGVNPpkLHRrq9ZcRTOFQ1EV-PjcbXnjdjalIqUVb2i4lBqL89K7sw6TiBalb6oYPx7YZIb5_rzNZi69aw0CE9MqjIjXK5o5MbsEU605fPLkrEhHcP_GPp91SaEnxd6BbUfDopuUKb12LaQQOXLgJpNTblkeHJBbMPdu6_MgbMNfU-H8j9lCRI4dg4sNkitajBM2Z3Eq8vZrkh-Fwn-GRca6T2mJPcRIe9Sn9VI5OaxeBbbzEeu7E8kWBxTqn7K7I4fJYcDqTlOs%2F5869c5c78f788%2Fca6b3cd22bbbdb89e49de7ce851e8ecfc200d6493735d4bc516946fe66d25fca%2Fmaster-playlist.m3u8&volume=39&muted=false&auto_quality=false&report=false&report_url=https%3A%2F%2Fyandex.ru%2Fsupport%2Fteletranslation%2Fform.html&preview=%2F%2F1.downloader.disk.yandex.ru%2Fpreview%2F78ea8fc7fa4e14f1e21b2ab6f3e62c5ce3e105079f6abd3f0bd46bd8ce501ee4%2Finf%2FHMoVfiu-IEyvBoWvMbw0mKlRa4PTak-7wDZFWCU8qQa8y5yXAjceU8l199hm1FNyAIHj0tolcKaMZUwYgpN-CQ%253D%253D%3Fuid%3D306257568%26filename%3DGame.of.Thrones.S08E01.Kings.Landing.1080p.AMZN.WEB-DL.DDP5.1.H.264-GoT.mkv%26disposition%3Dinline%26hash%3D%26limit%3D0%26content_type%3Dimage%252Fjpeg%26tknv%3Dv2%26size%3D1920x1080&host=disk.yandex.ru&additional_params=%7B%22from%22%3A%22other%22%2C%22vsid%22%3A%22ccdc885e8239533821edd81c407c107cf937faf8e4e67476e1deaa6648e6ced6%22%7D&vsid=ccdc885e8239533821edd81c407c107cf937faf8e4e67476e1deaa6648e6ced6',
   ]
 ]
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home container">
-    <h1>Game of thrones</h1>
+    <h1>Game of Thrones</h1>
     <div class="row season-grid mt-5">
       <div class="col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3" v-for="(season, i) in seasons" :key="i">
         <season-block :season="season" @click.native="clickSeason(i+1)"/>
